8e03d4e563cc4099032a936ad939c6c932f14231
Christian Westgaard <cwe@enonic.com>
2021-02-15 14:59:59 +0100

    Node 14.15.5

--- src/index.mjs
+++ src/index.mjs
@@ -11,29 +11,29 @@ export {
 	sort,
 	// validateField,
   visitAll
-} from './actions';
+} from './actions.es';
 
 export {
 	getEnonicContext,
 	EnonicContext
-} from './Context';
-export {Form} from './Form';
-export {List} from './List';
+} from './Context.jsx';
+export {Form} from './Form.jsx';
+export {List} from './List.jsx';
 
-export {DeleteButton} from './buttons/DeleteButton';
-export {DeleteItemButton} from './buttons/DeleteItemButton';
-export {InsertButton} from './buttons/InsertButton';
-export {MoveDownButton} from './buttons/MoveDownButton';
-export {MoveUpButton} from './buttons/MoveUpButton';
-export {ResetButton} from './buttons/ResetButton';
-export {SetValueButton} from './buttons/SetValueButton';
-export {SortButton} from './buttons/SortButton';
-export {SubmitButton} from './buttons/SubmitButton';
-export {ValidateFormButton} from './buttons/ValidateFormButton';
-export {VisitAllButton} from './buttons/VisitAllButton';
+export {DeleteButton} from './buttons/DeleteButton.jsx';
+export {DeleteItemButton} from './buttons/DeleteItemButton.jsx';
+export {InsertButton} from './buttons/InsertButton.jsx';
+export {MoveDownButton} from './buttons/MoveDownButton.jsx';
+export {MoveUpButton} from './buttons/MoveUpButton.jsx';
+export {ResetButton} from './buttons/ResetButton.jsx';
+export {SetValueButton} from './buttons/SetValueButton.jsx';
+export {SortButton} from './buttons/SortButton.jsx';
+export {SubmitButton} from './buttons/SubmitButton.jsx';
+export {ValidateFormButton} from './buttons/ValidateFormButton.jsx';
+export {VisitAllButton} from './buttons/VisitAllButton.jsx';
 
-export {Checkbox} from './inputs/Checkbox';
-export {Dropdown} from './inputs/Dropdown';
-export {Input} from './inputs/Input';
+export {Checkbox} from './inputs/Checkbox.jsx';
+export {Dropdown} from './inputs/Dropdown.jsx';
+export {Input} from './inputs/Input.jsx';
 
-export {deReference} from './utils/deReference';
+export {deReference} from './utils/deReference.es';
