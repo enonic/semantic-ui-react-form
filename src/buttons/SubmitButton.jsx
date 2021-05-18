2291f8392e504abe787ba962440749cb300e64e2
Christian Westgaard <cwe@enonic.com>
2021-05-18 11:13:50 +0200

    Make it possible to manually disable the Submit button

--- src/buttons/SubmitButton.jsx
+++ src/buttons/SubmitButton.jsx
@@ -5,7 +5,7 @@ import {getEnonicContext} from '../Context.jsx';
 import {submit} from '../actions.es';
 
 
-export function SubmitButton(/* props */) {
+export function SubmitButton(props) {
 	// console.debug('SubmitButton props', props);
 
 	const [context, dispatch] = getEnonicContext();
@@ -24,9 +24,13 @@ export function SubmitButton(/* props */) {
 	}, []);
  	// console.dir(errorsArr);
 
+	const {
+		disabled = !!errorsArr.length/* || !changesArr.length*/
+	} = props;
+
 	return <Button
 		color={errorsArr.length ? 'red' : 'green'}
-		disabled={!!errorsArr.length/* || !changesArr.length*/}
+		disabled={disabled}
 		onClick={() => dispatch(submit())}
 		type='submit'
 	><Icon name='save'/>{errorsArr.length ? `${errorsArr.length} validation error${errorsArr.length > 1 ? 's' : ''}`: 'Submit'}</Button>;
